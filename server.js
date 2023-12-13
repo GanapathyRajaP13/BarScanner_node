fd62694bee2058f7368b9caa5eea1c94590abc14
GanapathyRajaP13 <123737552+GanapathyRajaP13@users.noreply.github.com>
2023-12-13 13:02:49 +0530

    sql to mysql base config change

--- server.js
+++ server.js
@@ -1,62 +1,68 @@
-var express = require('express');
+var express = require("express");
 const cors = require("cors");
-const os = require('node:os');
+const os = require("node:os");
 var app = express();
 var PORT = 5500;
-os.hostname();
-console.log(os.hostname())
-app.use(
-    cors({
-      origin: "*",
-    })
-  );
-var sql = require("mssql");
 
+os.hostname();
+console.log(os.hostname());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+var sql = require("mysql2");
 
 // config for your database
 var config = {
-    user: 'root',
-    password: 'ganapa',
-    server: 'localhost',
-    database: 'scanData',
-    trustServerCertificate: true
+  user: "root",
+  password: "ganapa@13",
+  host: "localhost",
+  port: 3306,
+  database: "scanData",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 };
+
 sql.connect(config, function (err) {
-    if (err) console.log(err);
-    if (!err) console.log('Connection Established Successfully');
-})
-
-var server = app.listen(PORT,"", function (err) {
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", PORT);
+  if (err) console.log(err);
+  if (!err) console.log("Connection Established Successfully");
 });
 
-
-app.post('/getItemCode', function (req, res) {
-    var myFirstInput = req.query.id;
-    var request = new sql.Request();
-
-    request.query('Select b.PartnerProductNo from Inventory2 as a inner join PARTNER_PRODUCT as b on a.ProductID=b.ProductID where Container=' + '\'' + myFirstInput + '\'', function (err, recordset) {
-
-        if (err) console.log(err)
-
-        res.send(recordset);
-
-    });
+app.listen(PORT, function () {
+  console.log("Server is running on port " + PORT);
 });
 
-app.post('/getItemCodeforTata', function (req, res) {
-    var Input = req.query.id;
-    var request = new sql.Request();
+app.post("/getItemCode", function (req, res) {
+  var myFirstInput = req.query.id;
+  var request = new sql.Request();
 
-    request.query('Select * From WIP_SERIAL_NO_CONTENT where SerialNo=' + '\'' + Input + '\'', function (err, recordset) {
+  request.query(
+    "Select b.PartnerProductNo from Inventory2 as a inner join PARTNER_PRODUCT as b on a.ProductID=b.ProductID where Container=" +
+      "'" +
+      myFirstInput +
+      "'",
+    function (err, recordset) {
+      if (err) console.log(err);
 
-        if (err) console.log(err)
-
-        res.send(recordset);
-
-    });
+      res.send(recordset);
+    }
+  );
 });
 
+app.post("/getItemCodeforTata", function (req, res) {
+  var Input = req.query.id;
+  var request = new sql.Request();
 
+  request.query(
+    "Select * From WIP_SERIAL_NO_CONTENT where SerialNo=" + "'" + Input + "'",
+    function (err, recordset) {
+      if (err) console.log(err);
 
+      res.send(recordset);
+    }
+  );
+});
